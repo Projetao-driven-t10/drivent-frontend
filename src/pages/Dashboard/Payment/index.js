b842f4b4499fee1db0a69e84042c25a4f3c599fa
Cleiver <creibis10@gmail.com>
2023-08-01 16:18:12 -0300

    feat: layout estatico front end ingresso e pagamentos

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,36 @@
+import { PaymentPage, Title, Description, OptionBox, OptionType, OpetionPrice, OpetionContainer, ConfirmButton } from './style';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  return (
+    <PaymentPage>
+      <Title>Ingresso e Pagamento</Title>
+      <Description>Primeiro, escolha sua modalidade de ingresso</Description>
+      <OpetionContainer>
+        <OptionBox>
+          <OptionType>Presencial</OptionType>
+          <OpetionPrice>R$ 250</OpetionPrice>
+        </OptionBox>
+        <OptionBox>
+          <OptionType>Presencial</OptionType>
+          <OpetionPrice>R$ 250</OpetionPrice>
+        </OptionBox>
+      </OpetionContainer>
+
+      {/* Aparecer somente em caso de tipo de Hospedagem === Presencial */}
+      <Description>Ótimo! Agora escolha sua modalidade de Hospedagem</Description>
+      <OpetionContainer>
+        <OptionBox>
+          <OptionType>Sem hotel</OptionType>
+          <OpetionPrice>+ R$ 0</OpetionPrice>
+        </OptionBox>
+        <OptionBox>
+          <OptionType>Com Hotel</OptionType>
+          <OpetionPrice>+ R$ 250</OpetionPrice>
+        </OptionBox>
+      </OpetionContainer>
+
+      <Description>Fechado! O total ficou em <strong>R$ 600</strong>. Agora é só confirmar!</Description>
+      <ConfirmButton>RERSERVAR INGRESSO</ConfirmButton>
+    </PaymentPage>
+  );
 }
---
+++ src/pages/Dashboard/Payment/style.js
@@ -0,0 +1,93 @@
+import styled from 'styled-components';
+
+export const PaymentPage = styled.div`
+  padding: 15px;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Title = styled.h1`
+  width: 338px;
+  height: 40px;
+  font-family: Roboto;
+  font-size: 34px;
+  font-weight: 400;
+  line-height: 40px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #000000;
+`;
+
+export const Description = styled.p`
+  width: 479px;
+  height: 23px;
+  font-family: Roboto;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 23px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #8E8E8E;
+  margin-top: 30px;
+`;
+
+export const OptionBox = styled.div`
+  width: 145px;
+  height: 145px;
+  border-radius: 20px;
+  border: 1px solid #CECECE;
+  margin-top: 15px;
+  margin-right: 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  // recebe props
+  background-color: #FFEED2;
+`;
+
+export const OptionType = styled.p`
+  width: 75px;
+  height: 19px;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 19px;
+  letter-spacing: 0em;
+  text-align: center;
+`;
+
+export const OpetionPrice = styled.p`
+  width: 44px;
+  height: 16px;
+  font-family: Roboto;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 16px;
+  letter-spacing: 0em;
+  text-align: center;
+  color: #898989;
+  margin-top: 8px;
+`;
+
+export const OpetionContainer = styled.div`
+    width: auto;
+    height: auto;
+    display: flex;
+`;
+
+export const ConfirmButton = styled.button`
+    width: 182px;
+    height: 37px;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    font-family: Roboto;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 16px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: #000000;
+    margin-top: 12px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.05), 0 6px 20px 0 rgba(0, 0, 0, 0.1);
+`;
