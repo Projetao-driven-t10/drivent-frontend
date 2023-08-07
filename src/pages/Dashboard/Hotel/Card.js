7f6bbebd0e43ac5404713bb39e7ff176e7c240f7
Amanda Xavier <amandavxavier@gmail.com>
2023-08-07 12:41:31 -0300

    Feat: rooms capacity at hotel card

--- src/pages/Dashboard/Hotel/Card.js
+++ src/pages/Dashboard/Hotel/Card.js
@@ -1,12 +1,12 @@
 import { Card } from './style';
 
-export default function HotelCard({ name, image, rooms }) {
+export default function HotelCard({ name, image, rooms, roomsCapacity }) {
   return (
     <Card>
       <img src={image} alt='hotel' />
       <h6>{name}</h6>
       <p>Tipos de acomodação:</p>
-      <span>Single e Double</span>
+      <span>{roomsCapacity}</span>
       <p>Vagas disponíveis:</p>
       <span>{rooms.reduce((total, objetos) => total + objetos.capacity, 0)}</span>
     </Card>
