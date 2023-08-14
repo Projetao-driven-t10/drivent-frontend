e8478dd91e6f75967657f787d18f6f355b2133e9
Amanda Xavier <amandavxavier@gmail.com>
2023-08-14 17:28:38 -0300

    Fix: show rooms vacncies and selected room

--- src/pages/Dashboard/Hotel/Rooms.js
+++ src/pages/Dashboard/Hotel/Rooms.js
@@ -2,29 +2,46 @@ import { BsPerson, BsPersonFill } from 'react-icons/bs';
 import { Habitation, PersonIcon, SelectedPersonIcon } from './style';
 
 export default function Rooms({ id, name, capacity, vacancy, isSelected, setSelectedRoomId, selectedRoomId, selectedIconIndex, setSelectedIconIndex, setDisplayButton, setSelectedRoom, booking }) {
+  const remainingVacancies = capacity - booking.length;
+
   function handleChoice(id, icon) {
-    if(selectedIconIndex === icon && selectedRoomId === id) return;
-    setSelectedIconIndex(icon);
-    setSelectedRoomId(id);
-    setSelectedRoom({ name, capacity });
-    setDisplayButton(true);
-    console.log(booking);
-  };
+    if (selectedIconIndex === icon && selectedRoomId === id) {
+      // Se o ícone já estiver selecionado, desmarque-o
+      setSelectedIconIndex(null);
+      setSelectedRoomId(null);
+      setSelectedRoom(null);
+      setDisplayButton(false);
+    } else if (remainingVacancies > 0) {
+      // Se houver vagas, marque o ícone como selecionado e atualize o estado
+      setSelectedIconIndex(icon);
+      setSelectedRoomId(id);
+      setSelectedRoom({ name, capacity });
+      setDisplayButton(true);
+    }
+  }
+
   return (
-    <Habitation isSelected={isSelected} isAvailable={booking.length !== capacity} >
+    <Habitation isSelected={isSelected} isAvailable={remainingVacancies > 0}>
+      <span>{name}</span>
       <span>
-        {name}
-      </span>
-      <span>
-        {vacancy.map((v, i) => 
-          <PersonIcon 
-            key={i}
-            selected={selectedIconIndex === i} 
-            onClick={() => handleChoice(id, i)}>
-            {booking.length === capacity ? ( <BsPersonFill />) : (selectedIconIndex === v && selectedRoomId === id ? <SelectedPersonIcon /> : <BsPerson />)}
-          </PersonIcon>)}
+        {vacancy.map((v, i) => (
+          <PersonIcon key={i} selected={selectedIconIndex === i} onClick={() => handleChoice(id, i)}>
+            {i < booking.length ? (
+              <BsPersonFill />
+            ) : i < capacity ? (
+              remainingVacancies > 0 ? (
+                selectedIconIndex === i && selectedRoomId === id ? (
+                  <SelectedPersonIcon />
+                ) : (
+                  <BsPerson />
+                )
+              ) : (
+                <BsPersonFill />
+              )
+            ) : null}
+          </PersonIcon>
+        ))}
       </span>
     </Habitation>
   );
-};
-//booking.length > 0 ? (booking.length === capacity ? (<BsPersonFill />) : )
+}
