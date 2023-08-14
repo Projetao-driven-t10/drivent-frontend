1d0e1ed86839a7d1f5e4458a8d57ce4adb0275ca
suelen <sreginaclsilva@gmail.com>
2023-08-14 16:53:15 -0300

    style: no vacancy rooms

--- src/pages/Dashboard/Hotel/Card.js
+++ src/pages/Dashboard/Hotel/Card.js
@@ -1,11 +1,12 @@
 import { Card } from './style';
 
-export default function HotelCard({ id, name, image, rooms, roomsCapacity, setDisplayRooms, setBedrooms, isSelected, setSelectedHotelId, setSelectedHotel }) {
+export default function HotelCard({ id, name, image, rooms, roomsCapacity, setDisplayRooms, setBedrooms, isSelected, setSelectedHotelId, setSelectedHotel, booking }) {
   function chooseHotel(id) {
     setSelectedHotelId(id);
     setBedrooms(rooms);
     setSelectedHotel({ name, image });
     setDisplayRooms(true);
+    console.log(booking);
   }
   return (
     <Card onClick={() => chooseHotel(id)} isSelected={isSelected} >
